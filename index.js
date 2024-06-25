218e9a266c4556696fe53beeb5bc90407801c52e
Prati Patel <ppatel@marylandtaxes.gov>
2024-06-25 15:45:48 -0400

    changes

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
-console.log("Hello World");
 
-{/* <button type="button"
-onclick="document.getElementById('demo').innerHTML = Date()">
-Click me to display Date and Time.</button> */}
+console.log("Hello Prati");
